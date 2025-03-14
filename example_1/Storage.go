ab82c369a485430081f92b5c241940e0f8865ae3
Jared Wasinger <j-wasinger@hotmail.com>
2025-03-14 14:29:49 +0100

    update

--- example_1/Storage.go
+++ example_1/Storage.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"math/big"
 
@@ -15,6 +16,7 @@ import (
 
 // Reference imports to suppress errors if they are not otherwise used.
 var (
+	_ = bytes.Equal
 	_ = errors.New
 	_ = big.NewInt
 	_ = common.Big1
@@ -25,8 +27,8 @@ var (
 // StorageMetaData contains all meta data concerning the Storage contract.
 var StorageMetaData = bind.MetaData{
 	ABI: "[{\"inputs\":[],\"name\":\"retrieve\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"number\",\"type\":\"uint256\"}],\"name\":\"store\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]",
-	ID:  "Storage",
-	Bin: "0x6080604052348015600e575f80fd5b506101438061001c5f395ff3fe608060405234801561000f575f80fd5b5060043610610034575f3560e01c80632e64cec1146100385780636057361d14610056575b5f80fd5b610040610072565b60405161004d919061009b565b60405180910390f35b610070600480360381019061006b91906100e2565b61007a565b005b5f8054905090565b805f8190555050565b5f819050919050565b61009581610083565b82525050565b5f6020820190506100ae5f83018461008c565b92915050565b5f80fd5b6100c181610083565b81146100cb575f80fd5b50565b5f813590506100dc816100b8565b92915050565b5f602082840312156100f7576100f66100b4565b5b5f610104848285016100ce565b9150509291505056fea2646970667358221220bb1add3845e8d6556c69512a3badf1160140f85779530246e037c720d649074d64736f6c634300081a0033",
+	ID:  "e338b353ed1163ea35b3bea455d45b50d7",
+	Bin: "0x6080604052348015600e575f5ffd5b506101298061001c5f395ff3fe6080604052348015600e575f5ffd5b50600436106030575f3560e01c80632e64cec11460345780636057361d14604e575b5f5ffd5b603a6066565b60405160459190608d565b60405180910390f35b606460048036038101906060919060cd565b606e565b005b5f5f54905090565b805f8190555050565b5f819050919050565b6087816077565b82525050565b5f602082019050609e5f8301846080565b92915050565b5f5ffd5b60af816077565b811460b8575f5ffd5b50565b5f8135905060c78160a8565b92915050565b5f6020828403121560df5760de60a4565b5b5f60ea8482850160bb565b9150509291505056fea26469706673582212200857cb05506ae0a0b1cb7a5f2c713313775ab4ec0c0c8e02a0d5c9ce9dd77fd364736f6c634300081c0033",
 }
 
 // Storage is an auto generated Go binding around an Ethereum contract.
@@ -49,7 +51,8 @@ func (c *Storage) Instance(backend bind.ContractBackend, addr common.Address) *b
 	return bind.NewBoundContract(addr, c.abi, backend, backend, backend)
 }
 
-// Retrieve is a free data retrieval call binding the contract method 0x2e64cec1.
+// PackRetrieve is the Go binding used to pack the parameters required for calling
+// the contract method with ID 0x2e64cec1.
 //
 // Solidity: function retrieve() view returns(uint256)
 func (storage *Storage) PackRetrieve() []byte {
@@ -60,24 +63,25 @@ func (storage *Storage) PackRetrieve() []byte {
 	return enc
 }
 
+// UnpackRetrieve is the Go binding that unpacks the parameters returned
+// from invoking the contract method with ID 0x2e64cec1.
+//
+// Solidity: function retrieve() view returns(uint256)
 func (storage *Storage) UnpackRetrieve(data []byte) (*big.Int, error) {
 	out, err := storage.abi.Unpack("retrieve", data)
-
 	if err != nil {
 		return new(big.Int), err
 	}
-
 	out0 := abi.ConvertType(out[0], new(big.Int)).(*big.Int)
-
 	return out0, err
-
 }
 
-// Store is a free data retrieval call binding the contract method 0x6057361d.
+// PackStore is the Go binding used to pack the parameters required for calling
+// the contract method with ID 0x6057361d.
 //
 // Solidity: function store(uint256 number) returns()
-func (storage *Storage) PackStore(Number *big.Int) []byte {
-	enc, err := storage.abi.Pack("store", Number)
+func (storage *Storage) PackStore(number *big.Int) []byte {
+	enc, err := storage.abi.Pack("store", number)
 	if err != nil {
 		panic(err)
 	}
